679947a015daeb768331066b91824d40b1002ac1
Monosix8 <162055149+Monosix8@users.noreply.github.com>
2024-06-04 18:48:47 +0700

    awal program

--- exeptionStandard/exeptionStandard.cpp
+++ exeptionStandard/exeptionStandard.cpp
@@ -20,8 +20,4 @@ int main()
 		/*akan dieksekusi karna array data hanya
 		memiliki 3 elemen*/
 	}
-	cout << "Baris Program Yang terakhir" << endl;
-	/*penanda 2: bahwa program berjalan tanpa berhenti
-	meskipun terjadi kesalahan*/
-	return 0;
-}
+	
